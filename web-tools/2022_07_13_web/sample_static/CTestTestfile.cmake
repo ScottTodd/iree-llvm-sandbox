8cb24219e713320b3d554b7a36c75bfeb76be9b6
Scott Todd <scotttodd@google.com>
2022-08-10 08:27:43 -0700

    Add 2022_07_13 web demo.

---
+++ web-tools/2022_07_13_web/sample_static/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/local/google/home/scotttodd/code/iree/experimental/web/sample_static
+# Build directory: /usr/local/google/home/scotttodd/code/iree/build-emscripten/experimental/web/sample_static
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
